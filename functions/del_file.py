4654ed4475a1a65056b7ca9e8ccabc44602bf85f
DrPropperrR <228237@edu.fa.ru>
2024-05-31 16:44:19 +0300

    edit file

--- functions/del_file.py
+++ functions/del_file.py
@@ -5,7 +5,7 @@ def del_files(files: list) -> None:
     for name in files:
         if os.path.isfile(name):
             os.remove(name)
-            print(f'Файл {name} успешно удален')
+            print(f'Файл {name} удален')
 
         else:
             print(f'Файл {name} не существует')
